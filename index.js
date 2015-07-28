db380fe63fc9b0e63e0eaa7bd97e176ae3451be7
bcabanes <benjamin.cabanes@ugroupmedia.com>
2015-07-28 15:55:59 -0400

    [PROCESS] basic: can read file

--- index.js
+++ index.js
@@ -6,9 +6,45 @@ var request = require('request-promise');
 var stringify = require('json-stable-stringify');
 var through = require('through2');
 
+/**
+ * Constants.
+ */
+var PLUGIN_NAME = 'gulp-sync-loco';
 
-function GulpSynchLoco() {
+function gulpSyncLoco (options) {
+    options = options || {};
 
+    /**
+     * Set all needed variables
+     */
+    var firstFile;
+
+    /**
+     * Gulp Sync Loco start processing
+     */
+    return through.obj(function (file, enc, callback) {
+        if (file.isStream()) {
+            throw new gutil.PluginError(PLUGIN_NAME,
+                chalk.red('Streaming not supported'));
+        }
+
+        if (file.isNull()) {
+            // Return empty file.
+            callback(null, file);
+        }
+
+        if (!firstFile) {
+            firstFile = file;
+        }
+
+        if (file.isBuffer()) {
+            /**
+             * Start synchronizing with Loco
+             */
+            var content = file.contents.toString();
+            gutil.log(chalk.blue(content));
+        }
+    });
 }
 
-module.exports = GulpSynchLoco;
+module.exports = gulpSyncLoco;
