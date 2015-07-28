59bc9e0950685dff4c7a373085d96cfb032633b4
bcabanes <benjamin.cabanes@ugroupmedia.com>
2015-07-28 15:27:15 -0400

    [PROCESS] create index file

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+var _ = require('lodash');
+var chalk = require('chalk');
+var gutil = require('gulp-util');
+var request = require('request-promise');
+var stringify = require('json-stable-stringify');
+var through = require('through2');
+
+
+function GulpSynchLoco() {
+
+}
+
+module.exports = GulpSynchLoco;
